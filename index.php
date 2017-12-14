1e41c61853e28800a97d16a3b78b2bccc2cd5345
chandukumarM <34435013+chandukumarM@users.noreply.github.com>
2017-12-14 12:07:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,325 @@
+<!doctype html>
+<html>
+ <head>
+     <meta charset="utf-8">
+     <meta name="keywords"content="templete,free templete,boostraptemplete,">
+     <meta  name="author" content="chandu">
+     <meta name="discription" content="this is specially designed web templete">
+     <meta name="http-equiv" content="refresh">
+      <meta name="view-port" content="width=device-width,initial-scale=1.0" >
+    <title>task7</title>
+<link href="css/task7.css" rel="stylesheet" type="text/css">
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+ </head>
+
+      <body>
+	  <!----header--->
+	     <div class="topbar"><!-----topbar---->
+		       
+			   
+			   <div class="heading1">
+			      <h1>{warp}boostrap
+		       
+			   
+			   
+			   &nbsp <span>fly-responsive website templete</span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+				<button><a href="">Remove This Frame »  </a></button></h1>
+		       
+			   </div>
+		     
+			  
+		  
+		  
+		  </div>
+		  </div>
+		  
+		 
+		  
+		  
+		  
+		
+		  <div class="navbar">
+		              <div class="nam"><span>Miron</span>
+					  </div>
+					  
+					  <div class="navo">
+					  <ul class="navlist">
+					   <li><a href="">Home</a><i class="fa fa-angle-double-down" aria-hidden="true"></i></li>
+					    <li><a href="">Portfolio</a><i class="fa fa-angle-double-down" aria-hidden="true"></i></li>
+						<li><a href="about.html">Features</a><i class="fa fa-angle-double-down" aria-hidden="true"></i></li>
+						 <li><a href="">blog</a><ul class="dropdown">
+						<li><a href="#">Services-One</a></li>
+						<li><a href="#">Services-Two</a></li>
+						<li><a href="#">Services-Three</a></li>
+						<li><a href="#">Services-Four</a></li>
+					</ul><i class="fa fa-angle-double-down" aria-hidden="true"></i></li>
+					     <li ><a href="">Pages</a><i class="fa fa-angle-double-down" aria-hidden="true"></i></li>
+					 
+				    </ul>
+					
+					  </div>
+					  
+		  </div>
+
+		  <div class="container"><!-----slider---->
+		  <img class="slides"src="images/gggt.gif">
+		   
+			<button class="btn" id="btn2" >&#10094;</button>
+			<button class="btn" id="btn1" >&#10095;</button>
+		  </div>
+		
+		  
+		  <div class="sechead"><!----sectionheader--->
+		  Kumar HTML5 CSS3 Fully Responsive Website Templete
+		 <br>
+		 <span>withclean and valid code</span>
+		  </div>
+		  
+		  
+		  <div><!-----section content-->
+		  
+		  
+		   <div class="secicon">
+		    <div class="heart"><i class="fa fa-heart-o" ></i><br><span>
+			Make with Love</span></div>
+			 <div class="twitter"><i class="fa fa-twitter" ></i><br><span>Boostrap3</span></div>
+			  <div class="diamond"><i class="fa fa-diamond" ></i><br><span>Clean Code</span></div>
+			  <div class="map"><i class="fa fa-map-o" ></i><br><span>Fully responsive</span></div>
+		  </div>
+		  
+	 <div class="content">
+		   <div class="content1"> <p>
+		   The indefinite article when  .<br> It’s the word a  it precedes was <br>with a consonant. 
+		   It’word an when  <br>precede word that begins with<br> precedes a word that wiil be<br> that begins</p>
+		  </div>
+		  
+		
+		  <div class="content2"><p>
+		  The indefinite article when  .<br> It’s the word a  it precedes was <br>with a consonant. 
+		   It’word an when  <br>precede word that begins with<br> precedes a word that wiil be<br> that begins</p>
+		  </div>
+		  
+		  
+		   <div class="content3"><p>
+		   The indefinite article when  .<br> It’s the word a  it precedes was <br>with a consonant. 
+		   It’word an when  <br>precede word that begins with<br> precedes a word that wiil be<br> that begins</p>
+		  </div>
+		  
+		  <div class="content4"><p>
+		    The indefinite article when  .<br> It’s the word a  it precedes was <br>with a consonant. 
+		   It’word an when  <br>precede word that begins with<br> precedes a word that wiil be<br> that begins</p>
+		  </div>
+	 </div>
+		  
+		  </div>
+		  
+		  <div class= "clearfix1"><!--------clearfix---->
+		  </div>
+
+		  
+		  
+		  
+		  <div class="articleheader"><!-------articleheader----->
+		   Recently work
+		   <br>
+		   <i class="fa fa-picture-o"></i>
+		   <br>
+		   
+		      <div  class="clear"><!-----clearfix---->
+			  </div>
+		   
+		   
+		   
+		   
+		   <div class="articlemenu"><!-------articlelist--->
+		   
+		   
+		   <button>all</button>
+		   <button>design</button>
+		   <button>HTML5</button>
+		   <button>Wordpress</button>
+		   <button>Seo</button>
+		   
+		   
+		  
+		  </div>
+		  
+		  
+		  </div>
+		    <div class= "clearfix1"><!--------clearfix---->
+		  </div>
+		 
+
+		 
+		      <div class="articleimage"><!--------articleimages------>
+		  <img class="img" src="images/r1.jpg"/>
+		  <img class="img" src="images/r2.jpg"/>
+		  <img class="img" src="images/r3.jpg"/>
+		      </div>
+			  
+			  
+		      <div class="articleimage1"><!--------articleimages------>
+		   <img class="img" src="images/r4.jpg"/>
+		  <img class="img" src="images/r5.jpg"/>
+		  <img class="img" src="images/r6.jpg"/>
+		     </div>
+			 <div class= "clearfix1"><!--------clearfix---->
+		  </div>
+		   
+		   <div class="portfolio"><!---viewportfolio---->
+		  <div class="portfoliogap"> <span><a href="">View Portfolio</a></span></div>
+		  </div>
+		  
+		  
+		  <div class="yellowsection"><!----yellowsection---->
+		  <br>
+		  <h1>About Us</h1>
+		  <br>
+		  <i class="fa fa-users"></i>
+		  <br>
+		  <p>An article is a word used to modify a noun, which is a person, place, object, or idea. Technically,
+		  an article is an adjective, which is any word that modifies a noun. Usually adjectives modify nouns through description,
+		  but articles are used instead to point out or refer to nouns. There are two different types of articles that we use in </p>
+
+		  
+		   <div class="inlineicons">
+		   <ul>
+		  <li> <i class="fa fa-info-circle" aria-hidden="true"></i></li>
+		  <li><i class="fa fa-motorcycle" aria-hidden="true"></i></li>
+		 <li> <i class="fa fa-paper-plane" aria-hidden="true"></i></li>
+		<li> <i class="fa fa-newspaper-o" aria-hidden="true"></i></li>
+		 <li> <i class="fa fa-phone" aria-hidden="true"></i></li>
+		 <li> <i class="fa fa-plug" aria-hidden="true"></i></li>
+		 <li> <i class="fa fa-plane" aria-hidden="true"></i></li>
+		  <li><i class="fa fa-moon-o" aria-hidden="true"></i></li>
+		  </ul>
+		  </div>
+		  
+		  
+		  
+		  
+		  </div>
+		  
+		  
+		  <div class="news"><!-----latestnews--->
+		  <br>
+		  <h1>Latest News</h1>
+		  <br>
+		  <i class="fa fa-pencil-square-o" ></i>
+		  </div>
+		  
+		  
+		  <div class="btmimage"><!-----bottomimages--->
+		   <div class="btmimage1"> 
+		   <img class="img" src="images/r8.jpg"/>
+		  <img class="img" src="images/r9.jpg"/>
+		  <img class="img" src="images/r10.jpg"/>
+		  </div>
+		  </div>
+		  
+		  <div class= "clearfix1"><!--------clearfix---->
+		  </div>
+		   <div class= "clearfix1"><!--------clearfix---->
+		  </div>
+		 
+		  
+		    <div class="btncontent"><!-----bottomcontent--->
+			<div class="bt">
+			<div class="btmcc">
+			<h4>Amazing Theme for  Multi-purpose</h4><br>
+			<p>Articles are used to indicate whether <br>a noun in a sentence is specific or general.</p>
+			</div>
+			<div class="btmcc">
+			<h4>Amazing Theme for  Multi-purpose</h4><br>
+			<p>Articles are used to indicate whether <br>a noun in a sentence is specific or general.</p>
+			</div>
+			<div class="btmcc">
+			<h4>Amazing Theme for  Multi-purpose</h4><br>
+			<p>Articles are used to indicate whether <br>a noun in a sentence is specific or general.</p>
+			</div>
+			</div>
+			
+			</div>
+
+			 <div class= "clearfix1"><!--------clearfix---->
+		  </div>
+		  <br>
+		  <br>
+		  <br>
+
+		  
+		  
+		  
+		  
+		  
+		    <div class="footer"><!-----footer--->
+			
+			<br>
+			<br>
+			<br>
+			<br>
+			<div class="fheading"><!----footer heading--->
+
+			</div>
+			<br>
+			<br>
+			  <div class="fcontent"><!-----footerbodies--->
+			     <div class="f1">
+				 			<h3>About Us</h3>
+				 <p>The indefinite article takes two forms. It’s the word a when it precedes a word that begins with a consonant
+				 . It’s the word an when it precedes a word that begins with a vowel. The indefinite article indicates 
+				 that a noun refers to a general idea rather than a particular thing. For example, you might ask your friend, 
+				 Your friend will understand that you are not asking about a specific type of gift or a specific item. “</p>
+			     </div>
+				 
+				 <div class="f2">
+				 <h3>Latest Photos</h3>
+				 list item<br><hr>
+				  list item<br><hr>
+				   list item<br><hr>
+				    list item<br><hr>
+					 list item<br><hr>
+			     </div>
+				 
+				 <div class="fimage">
+				 <h3>Latest Work</h3>
+				 
+				  <img class="img1" src="images/r1.jpg"/>
+		  <img class="img1" src="images/r2.jpg"/>
+		  <img class="img1" src="images/r3.jpg"/>
+		  	 
+				  <img class="img1" src="images/r1.jpg"/>
+		  <img class="img1" src="images/r2.jpg"/>
+		  <img class="img1" src="images/r3.jpg"/>
+			     </div>
+				 
+				 <div class="contact">
+				 <h3>Get in Touch</h3>
+				 <i class="fa fa-home"></i>  :124,madhapur ,hyderabad telangana<br>
+				  <li> <i class="fa fa-phone" ></i> :Phone:+91 7095220401<br>
+				  <li> <i class="fa fa-bell" ></i>  :Email:chandu3319@gamil.com<br>
+				  <li> <i class="fa fa-bullhorn" ></i> :Website:www.zerocost.com<br>
+				  <i class="fa fa-facebook-official" aria-hidden="true"></i>
+				  <i class="fa fa-twitter-square" aria-hidden="true"></i>
+				  <i class="fa fa-google-plus-square" aria-hidden="true"></i>
+				  <i class="fa fa-instagram" aria-hidden="true"></i>
+			     </div>
+		     </div>
+		  
+              <div  class="clear"><!-----clearfix---->
+			  </div>
+			  
+			  <div class="copyright"><!-----copyright--->
+			  <br>
+			  <br>
+			  <h1>Kuma<span>r:</span></h1>
+			  <p><i class="fa fa-copyright" aria-hidden="true"></i> Copyright 2017.All rights reserved.</p>
+		      </div>
+			
+		  </div>
+		  
+		  
+      </body>
+
+</html>
